9770e13f3a503aa7a535c9851d99d31133c6636a
Ali Herawi <aliherawi7@gmail.com>
2024-01-30 09:17:41 +0430

    favorite list updated

--- script.js
+++ script.js
@@ -1,124 +1,171 @@
-
-//  music files
-
-let musicfiles=[];
-for(let x =0; x < 8; x++){
-    musicfiles.push(new Audio(`music/${x+1}.mp3`));
-}
-let playingAudio = musicfiles[0];
-
-
+// general button
+const playbtn = document.getElementById('playbtn');
 const next = document.getElementById('next');
-const prev = document.getElementById('prev'); 
+const prev = document.getElementById('prev');
 const musicList = document.querySelector('.music-list');
 const playListWidth = document.getElementsByClassName('music');
 let musicItems = document.getElementsByClassName('music');
 
+
+
+
+//  music files
+
+let musicfiles = [];
+for (let x = 0; x < 8; x++) {
+    musicfiles.push(new Audio(`music/${x + 1}.mp3`));
+}
+let playingAudio = musicfiles[0];
+
+
+//  <div class="music" id="music1">
+// <h4>Music 1</h4>
+// <div class="add-to-fav">
+//     <i class="bi bi-heart"></i>
+// </div>
+// </div>
+
+
+const initializeMusicList = (size) => {
+    let musicListMain = document.getElementById("music_list");
+    // console.log(musicFiles.size)
+    for (let i = 0; i < size; i++) {
+        let div = document.createElement("div")
+        div.classList.add("music")
+        div.id = "music_" + (i + 1)
+
+        let h4 = document.createElement("h4")
+        h4.textContent = "Music " + (i + 1);
+
+        let nestedDiv = document.createElement("div")
+        nestedDiv.classList.add("add-to-fav")
+        let icon = document.createElement("i")
+        icon.classList.add("bi", "bi-heart")
+
+        icon.setAttribute("data", (i))
+
+        nestedDiv.appendChild(icon);
+
+        div.appendChild(h4)
+        div.appendChild(nestedDiv)
+
+        musicListMain.appendChild(div)
+    }
+    for (let x = 0; x < musicItems.length; x++) {
+        musicItems[x].addEventListener('click', () => {
+            playanimation();
+            playbtn.click();
+            playingAudio.load();
+            playingAudio = musicfiles[x];
+            playbtn.click();
+            playingAudio.loop = true;
+            playbtn.style.backgroundImage = musicItems[x].style.backgroundImage;
+        });
+    }
+
+    initializeBackgroundFirst();
+}
+
+
+// first initialize
+function initializeBackgroundFirst() {
+    playbtn.style.backgroundImage = `url(\'image/${1}.jpg\')`;
+    for (let x = 0; x < musicItems.length; x++) {
+        musicItems[x].style.backgroundImage = `url(\'image/${x + 1}.jpg\')`;
+    }
+}
+
+
+
+// initilize the music list
+initializeMusicList(musicfiles.length);
+
+
+
+
+
+
+// music list slider 
+
 let allWidth = 0;
-for(let x =0; x < playListWidth.length; x++){
+for (let x = 0; x < playListWidth.length; x++) {
     allWidth += 150;
 }
 let transitionWidth = 0;
-next.addEventListener('click', () =>{
-    if( transitionWidth +(4*150) > allWidth){
+next.addEventListener('click', () => {
+    if (transitionWidth + (4 * 150) > allWidth) {
         return;
     }
     transitionWidth += 150;
     musicList.style.transform = `translateX(-${transitionWidth}px)`;
-},false);
+}, false);
 
-prev.addEventListener('click', () =>{
-    if( transitionWidth ==0){
+prev.addEventListener('click', () => {
+    if (transitionWidth == 0) {
         return;
     }
     transitionWidth -= 150;
     musicList.style.transform = `translateX(-${transitionWidth}px)`;
-},false);
+}, false);
 
 // playing actions
 
-const playbtn = document.getElementById('playbtn');
-playbtn.addEventListener('click',() =>{
-    if(playbtn.firstElementChild.classList.contains('bi-play-fill')){
-        playbtn.firstElementChild.classList.replace('bi-play-fill','bi-pause-fill');
+playbtn.addEventListener('click', () => {
+    if (playbtn.firstElementChild.classList.contains('bi-play-fill')) {
+        playbtn.firstElementChild.classList.replace('bi-play-fill', 'bi-pause-fill');
         playanimation();
         playingAudio.play();
-        playingAudio.volume = volumeSize.value/100;
-        
-    }else{
-        playbtn.firstElementChild.classList.replace('bi-pause-fill','bi-play-fill');
+        playingAudio.volume = volumeSize.value / 100;
+
+    } else {
+        playbtn.firstElementChild.classList.replace('bi-pause-fill', 'bi-play-fill');
         stopAnimation();
         playingAudio.pause();
     }
-    
-    
-
 })
 
 
 // clock on play view
-let clock  = document.querySelector('.clock-info');
-(function (){
-    window.setInterval(getTime,1000);
+let clock = document.querySelector('.clock-info');
+(function () {
+    window.setInterval(getTime, 1000);
 }());
 
-function getTime(){
+function getTime() {
     let date = new Date();
     let datname;
-    switch(date.getDay()){
-        case 0 : datname = 'Sun';
-                break;
-        case 1 : datname = 'Mon';
-                break;
-        case 2 : datname = 'Tue';
-                break;
-        case 3 : datname = 'Wed';
-                break;
-        case 4 : datname = 'Thu';
-                break;
-        case 5 : datname = 'Fri';
-                break;
-        case 6 : datname = 'Sat';
-                break;   
+    switch (date.getDay()) {
+        case 0: datname = 'Sun';
+            break;
+        case 1: datname = 'Mon';
+            break;
+        case 2: datname = 'Tue';
+            break;
+        case 3: datname = 'Wed';
+            break;
+        case 4: datname = 'Thu';
+            break;
+        case 5: datname = 'Fri';
+            break;
+        case 6: datname = 'Sat';
+            break;
     }
-    let dateStr = (datname) + ". "+(date.getHours())+":"+date.getMinutes() +":"+date.getSeconds();
+    let dateStr = (datname) + ". " + (date.getHours()) + ":" + date.getMinutes() + ":" + date.getSeconds();
     clock.innerHTML = dateStr;
-}
-
-initializeFirst();
-
-// first initialize
-function initializeFirst(){
-    playbtn.style.backgroundImage = `url(\'image/${1}.jpg\')`;
-    for(let x = 0; x < musicItems.length; x++){
-        musicItems[x].style.backgroundImage = `url(\'image/${x+1}.jpg\')`;
-    }
-}
-
-for(let x=0; x < musicItems.length; x++){
-    musicItems[x].addEventListener('click',() => {
-        playanimation();
-        playbtn.click();
-        playingAudio.load();
-        playingAudio = musicfiles[x];
-        playbtn.click();
-        playingAudio.loop = true;
-        playbtn.style.backgroundImage = musicItems[x].style.backgroundImage;
-    });
 }
 
 
 
 // playing animation
 let animate = document.querySelectorAll('.lines li');
-function playanimation(){
-    animate.forEach((value)=>{
+function playanimation() {
+    animate.forEach((value) => {
         value.style.animationName = `animate2`;
     })
-    
+
 }
-function stopAnimation(){
-    animate.forEach((value)=>{
+function stopAnimation() {
+    animate.forEach((value) => {
         value.style.animationName = `animateuyt2`;
     })
 }
@@ -129,9 +176,9 @@ let volumeSize = document.querySelector('.sounds input');
 volumeSize.addEventListener('change', volumeChange, false);
 
 console.dir(document.querySelector('.sounds input'))
-function volumeChange(){
-    document.querySelector('.sounds input').addEventListener('pointermove',function(){
-        playingAudio.volume = volumeSize.value/100;
+function volumeChange() {
+    document.querySelector('.sounds input').addEventListener('pointermove', function () {
+        playingAudio.volume = volumeSize.value / 100;
     });
 
 }
@@ -141,50 +188,103 @@ function volumeChange(){
 
 
 // setting 
-document.querySelector('.btnsetting').addEventListener('click',() =>{
+document.querySelector('.btnsetting').addEventListener('click', () => {
     document.querySelector('.setting').style.transform = 'scaleX(1)';
 });
 
-document.querySelector('#back-setting').addEventListener('click',() => {
+document.querySelector('#back-setting').addEventListener('click', () => {
     document.querySelector('.setting').style.transform = 'scaleX(0)';
 })
 
 let selectOp = document.querySelector('select');
 selectOp.addEventListener('change', () => {
     let color = (selectOp[selectOp.selectedIndex].innerHTML);
-    let allIcons = document.querySelectorAll('i');
-    el.forEach((value)=>{
-        value.style.color = `${color}`;
-    }) 
+    document.body.style.color = color;
 
-    let allH4 = document.querySelectorAll('h4');
-    allH4.forEach((element)=>{
-        element.style.color = `${color}`;
-    })
+    //  document.querySelector('input').background =  `${color}`;
 
-    document.querySelector('input').background =  `${color}`;
 
-    
 });
 
 // playelist button
-let playListBtn = document.getElementById("play-list");
-playListBtn.addEventListener('click',() =>{
-    document.querySelector(".play-list").classList.toggle("bottom-130");
-    document.querySelector(".music-player-body").classList.toggle("translateY-70");
+let playListBtn = document.getElementById("play_list");
+playListBtn.addEventListener('click', () => {
     playListBtn.classList.toggle("btn-active")
-    
+
 })
+
+
+
+
+let fovriteMusic = new Set();
 
 // add to favorite list
 let favIcons = document.querySelectorAll(".add-to-fav i");
-favIcons.forEach((icon)=>{
-    icon.addEventListener("click", (e)=>{
+favIcons.forEach((icon) => {
+    icon.addEventListener("click", (e) => {
         e.stopPropagation()
         icon.classList.toggle("bi-heart");
         icon.classList.toggle("bi-heart-fill");
-        
+        const index = icon.getAttribute("data");
+
+        fovriteMusic.add(musicfiles[index])
+        console.log(fovriteMusic)
         // adding music to favorite list
-        
+
     })
 })
+
+// fovrite list music show up
+
+let fovriteButton = document.getElementById("fovrite_button");
+
+fovriteButton.onclick = () => {
+    clearList();
+    console.log(fovriteMusic)
+    initializeMusicList(fovriteMusic.size);
+}
+
+
+// list music show up
+
+
+let playListButton = document.getElementById("play_list");
+
+playListButton.onclick = () => {
+    clearList();
+    initializeMusicList(musicfiles.length);
+}
+
+
+
+
+
+// make the list empty
+
+function clearList() {
+    let innerList = document.getElementsByClassName("inner-list")[0];
+    let musicList = document.getElementById("music_list");
+    if (musicList) {
+        innerList.removeChild(musicList);
+        musicList = document.createElement("div")
+        musicList.id = "music_list";
+        musicList.classList.add("music-list")
+        innerList.appendChild(musicList);
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
